494e7057560a84ba21fdb64feed3057ddba2f728
IDPantonioivan <ffx_wakka@hotmail.com>
2016-03-09 20:05:01 +0000

    Update deleteusers.rb

--- trim1/u1/Script delete-users/deleteusers.rb
+++ trim1/u1/Script delete-users/deleteusers.rb
@@ -1,14 +1,32 @@
 #!/usr/bin/ruby
+# encoding: utf-8
 
-users = `cat userlist.txt`
+#Comprobar si el usuario es root
+def comprobar_root
 
-userlist = users.split("\n")
+usuario=`whoami`
+quiensoy=usuario.chop
 
-userlist.each do
-
-{|nombre| system("userdel -f -r #{nombre}")}
+if quiensoy=="root" then
+	puts "Eres usuario root, todo correcto"
+	puts " "
+else
+	puts "deber ser root para ejecutar este script"
+	puts " "
+	exit
+end
 
 end
 
-puts "Borrados los usuarios"
+#Listamos los usuarios y luego los eliminamos
+def eliminar_usuarios
+lista=`cat userslist.txt`
+usuarios=lista.split("\n")
+usuarios.each do |nombre|
+system("userdel -f -r #{nombre}")
+end
+end
 
+#Llamamos a ambas funciones
+comprobar_root
+eliminar_usuarios
